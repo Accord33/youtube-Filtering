8d62dbd5eaaa2cbb30ba60f51a064eac9562c884
KomatsuzakiAruki <kmtszkkr@gmail.com>
2023-12-02 03:29:37 +0900

    delete shorts

---
+++ deleteshorts.js
@@ -0,0 +1,7 @@
+window.onload = function(){
+    //let shortsMenu = document.querySelector("#guide-button");//左上の３本線のアイコン
+    let shortsTab = document.querySelector("#items > ytd-mini-guide-entry-renderer:nth-child(2)");//左のサイドメニューのショートのアイコン
+   
+    //shortsMenu.style.display = "none";
+    shortsTab.style.display = "none";
+ }
