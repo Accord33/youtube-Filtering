e85887d5ad891d48729b4bb0a499b1a272ff01c4
KomatsuzakiAruki <kmtszkkr@gmail.com>
2023-12-02 14:22:49 +0900

    delete shorts icon in menu

---
+++ deleteshorts.js
@@ -0,0 +1,10 @@
+window.onload = function(){
+    let shortsTab = document.querySelector("#items > ytd-mini-guide-entry-renderer:nth-child(2)");//左のサイドメニューのショートのアイコン
+    shortsTab.style.display = "none";
+ };
+
+ let menuicon = document.querySelector("#guide-button");//ハンバーガーメニューボタン
+ menuicon.addEventListener('click',() => {
+    let shortsInMeun = document.querySelector("#items > ytd-guide-entry-renderer:nth-child(2)");//メニュー内のショートボタン
+    shortsInMeun.style.display = "none";
+ });
