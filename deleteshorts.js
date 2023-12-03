6ebc33061b6e7cb3fde6e44e29a12599115f4c0a
Staycia930 <skb.ceg.biblia@gmail.com>
2023-12-03 13:27:49 +0900

    reshape code

--- deleteshorts.js
+++ deleteshorts.js
@@ -1,10 +1,11 @@
 window.addEventListener('load',() => {
+
     let shortsTab = document.querySelector("#items > ytd-mini-guide-entry-renderer:nth-child(2)");//左のサイドメニューのショートのアイコン
     shortsTab.style.display = "none";
- });
+});
 
- let menuicon = document.querySelector("#guide-button");//ハンバーガーメニューボタン
- menuicon.addEventListener('click',() => {
+let menuicon = document.querySelector("#guide-button");//ハンバーガーメニューボタン
+menuicon.addEventListener('click',() => {
     let shortsInMeun = document.querySelector("#items > ytd-guide-entry-renderer:nth-child(2)");//メニュー内のショートボタン
     shortsInMeun.style.display = "none";
- });
+});
